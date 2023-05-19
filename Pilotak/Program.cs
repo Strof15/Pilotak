a92f715a07148b62de9b5c818fcd982da3c0d5cc
Kristof <Kristof@DESKTOP-C9STJNN>
2023-05-19 10:35:38 +0200

    idaig jutottam eddig

--- Pilotak/Program.cs
+++ Pilotak/Program.cs
@@ -10,18 +10,39 @@ namespace Pilotak
             Beolvasas();
             harmasfeladat();
             negyesfeladat();
+            otosfeladat();
         }
         static List<Class1> pilotatomb = new List<Class1>();
-            static void Beolvasas()
+        
+        static List<string> valami;
+
+        static void Beolvasas()
             {
                 StreamReader olvas = new StreamReader("pilotak.csv");
                 olvas.ReadLine();
+                pilotatomb.Take(1);            
                 while (!olvas.EndOfStream)
                 {
                     string[] adat = olvas.ReadLine().Split(';');
-            
+                    
+
+                if (adat[3] == "") 
+                {
+                    adat[3] = "";
                     pilotatomb.Add(new Class1(adat[0], adat[1], adat[2], int.Parse(adat[3])));
+                   
                 }
+                else
+                {
+                    pilotatomb.Add(new Class1(adat[0], adat[1], adat[2], int.Parse(adat[3])));
+                    
+                }
+                     valami = adat[1].Split(".").ToList();
+
+                }
+
+               
+
                 olvas.Close();
 
             }
@@ -34,7 +55,22 @@ namespace Pilotak
         static void negyesfeladat()
         {
             Console.WriteLine($"4. feladat: {pilotatomb.Last().Nev}");
-         }
+        }
+
+        static void otosfeladat()
+        {
+            
+            Console.WriteLine("5. feladat:");
+           
+            string[] otoshoztomb = pilotatomb.Count.ToString().Split(".");
+            if (valami[0])
+            {
+                
+
+            }
+
+
+        }
 
 
 
