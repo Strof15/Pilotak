56f7a5fe85fcc4a85824a3f69e242b76beaeb2c3
Kristof <Kristof@DESKTOP-C9STJNN>
2023-05-19 10:01:04 +0200

    Szunet elotti mentes

---
+++ Pilotak/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilotak
+{
+    internal class Class1
+    {
+        string nev;
+        string szuldate;
+        string nemzetiseg;
+        int rajtszam;
+
+        public Class1(string nev, string szuldate, string nemzetiseg, int rajtszam)
+        {
+            this.nev = nev;
+            this.szuldate = szuldate;
+            this.nemzetiseg = nemzetiseg;
+            this.rajtszam = rajtszam;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public string Szuldate { get => szuldate; set => szuldate = value; }
+        public string Nemzetiseg { get => nemzetiseg; set => nemzetiseg = value; }
+        public int Rajtszam { get => rajtszam; set => rajtszam = value; }
+    }
+}
--- Pilotak/Program.cs
+++ Pilotak/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+namespace Pilotak 
+{
+    internal class prog 
+    {
+        static void main(string[] args) 
+        {
+            Beolvasas();
+            harmasfeladat();
+            negyesfeladat();
+        }
+        static List<Class1> pilotatomb = new List<Class1>();
+            static void Beolvasas()
+            {
+                StreamReader olvas = new StreamReader("pilotak.csv");
+                olvas.ReadLine();
+                while (!olvas.EndOfStream)
+                {
+                    string[] adat = olvas.ReadLine().Split(';');
+            
+                    pilotatomb.Add(new Class1(adat[0], adat[1], adat[2], int.Parse(adat[3])));
+                }
+                olvas.Close();
+
+            }
+
+        static void harmasfeladat() 
+        {
+            Console.WriteLine($"3. feladat: {pilotatomb.Count}");
+        }
+
+        static void negyesfeladat()
+        {
+            Console.WriteLine($"4. feladat: {pilotatomb.Last().Nev}");
+         }
+
+
+
+
+
+        
+    
+    
+    
+    }
+
+
+}
